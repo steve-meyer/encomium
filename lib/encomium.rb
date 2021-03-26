e8e5d2c2be90c09805dee8eef09e7cbdde98af7e
Steve Meyer <stephen.meyer@wisc.edu>
2021-03-26 10:12:28 -0500

    Adding ISSN validation

--- lib/encomium.rb
+++ lib/encomium.rb
@@ -4,5 +4,10 @@ require_relative "encomium/version"
 
 module Encomium
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def valid_issn?(issn)
+      issn.is_a?(String) && (issn =~ /^[0-9]{4}-[0-9]{3}[0-9xX]$/) == 0
+    end
+  end
 end
--- spec/encomium_spec.rb
+++ spec/encomium_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe Encomium do
     expect(Encomium::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "when validating ISSNs" do
+    it "considers 4 digits, dash, 4 digits valid" do
+      expect(Encomium.valid_issn?("1234-5678")).to be true
+    end
+
+    it "considers a number to be invalid" do
+      expect(Encomium.valid_issn?(12345678)).to be false
+    end
+
+    it "considers 4 digits, dash, uppercase X valid" do
+      expect(Encomium.valid_issn?("1234-567X")).to be true
+    end
+
+    it "considers 4 digits, dash, lowercase x valid" do
+      expect(Encomium.valid_issn?("1234-567x")).to be true
+    end
   end
 end
